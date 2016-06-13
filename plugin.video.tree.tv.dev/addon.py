f92ffa1621813fb73586f5ab27a72665f8f2fbb5
chimkentec <chimkentec@gmail.com>
2016-06-13 11:50:51 +0600

    1q
    d

---
+++ plugin.video.tree.tv.dev/addon.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import xbmcup.app
+from core.index import Index
+from core.list import MovieList, BookmarkList, QualityList, SearchList, CollectionList
+from core.filter import Filter
+from core.context import ContextMenu
+
+plugin = xbmcup.app.Plugin()
+
+plugin.route(None, Index)
+plugin.route('list', MovieList)
+plugin.route('quality-list', QualityList)
+plugin.route('search', SearchList)
+plugin.route('collection', CollectionList)
+plugin.route('filter', Filter)
+plugin.route('bookmarks', BookmarkList)
+plugin.route('context', ContextMenu)
+
+plugin.run()
